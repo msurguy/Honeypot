a38fb12eaf84952651c58d46ef24b21d90108a67
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-10-21 22:03:38 -0500

    Removing unnecessary text

--- src/lang/es/validation.php
+++ src/lang/es/validation.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-    'honeypot'  => 'Se ha detectado SpamSpam has been detected',
+    'honeypot'  => 'Se ha detectado Spam',
     'honeytime' => 'Por favor espera antes de enviar el formulario de nuevo',
 );
